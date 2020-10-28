87d4784440e601b758d5fa716701f23c5b793884
Luis Plata <www.luisplata@gmail.com>
2020-10-27 23:20:36 -0500

    Se realiza la primera version de la puntuacion. falta evitar que caiga en el bucleInfinito

--- Assets/Scripts/Flores/ColocandoFlores.cs
+++ Assets/Scripts/Flores/ColocandoFlores.cs
@@ -6,9 +6,12 @@ public class ColocandoFlores : MonoBehaviour
 {
     [SerializeField] private List<string> tagsDeFlores;
     private Dictionary<string, List<GameObject>> listasDeFlores;
+    private List<int> posicionesYaTomadas;
     
     private void Start()
     {
+        posicionesYaTomadas = new List<int>();
+        listasDeFlores = new Dictionary<string, List<GameObject>>();
         foreach (string tagFlor in tagsDeFlores)
         {
             List<GameObject> lista = new List<GameObject>();
@@ -25,5 +28,16 @@ public class ColocandoFlores : MonoBehaviour
     {
         listasDeFlores.TryGetValue(tagDeLaFlor, out List<GameObject> listaRecuperada);
 
+        int posicionParaTomar = 0;
+        do
+        {
+            posicionParaTomar = Random.Range(0, listaRecuperada.Count - 1);
+        } while (posicionesYaTomadas.Contains(posicionParaTomar) && posicionesYaTomadas.Count < listaRecuperada.Count);
+
+        posicionesYaTomadas.Add(posicionParaTomar);
+
+        GameObject referencia = listaRecuperada[posicionParaTomar];
+        referencia.GetComponent<MeshRenderer>().enabled = true;
+        Debug.Log("Instancio");
     }
 }
--- Assets/Scripts/Flores/ControladorDePuntuacion.cs
+++ Assets/Scripts/Flores/ControladorDePuntuacion.cs
@@ -2,8 +2,9 @@
 
 public class ControladorDePuntuacion : MonoBehaviour
 {
+    [SerializeField] private ColocandoFlores colocadorDeFlores;
     public void AumentoDePuntuacion(FlorDelMapa florTocada)
     {
-        
+        colocadorDeFlores.ColocarFlor(florTocada.gameObject.tag);
     }
 }
