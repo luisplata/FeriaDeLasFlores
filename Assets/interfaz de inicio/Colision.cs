3980125e1f35a67604515d8052e3f3dcff98351d
Luis Plata <www.luisplata@gmail.com>
2020-10-28 18:48:01 -0500

    Se termina la pantalla de inicio, con una animacion y el boton para jugar. No tiene funcionalidad el boton salir porque es para web inicialmente

---
+++ Assets/interfaz de inicio/Colision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Colision : MonoBehaviour
+{
+    [SerializeField] private Button botonJugar;
+    [SerializeField] private int escenaCargar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        botonJugar.onClick.AddListener(delegate { AcccionDeBoton(); });
+    }
+
+    private void AcccionDeBoton()
+    {
+        SceneManager.LoadScene(escenaCargar);
+    }
+  
+}
