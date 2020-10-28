1f66381db7b9c58807eeaf1469a5d59b31b42fe1
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2020-10-28 13:46:05 -0500

    trains

---
+++ Assets/Train.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    [SerializeField]
+    private bool invertMovement;
+
+    [SerializeField]
+    private float movementSpeed = -10f;
+
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        movementSpeed *= invertMovement ? -1 : 1;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(new Vector3(0, 0, movementSpeed), ForceMode.Impulse);
+    }
+
+}
